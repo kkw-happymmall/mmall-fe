31ae29197b814667ba7ba7c8b6057b97f4523729
Wendy Shu <qianwenshu_isun@163.com>
2017-07-04 14:24:45 +0800

    环境配置，项目搭建

---
+++ src/page/common/index.js
@@ -0,0 +1,10 @@
+/*
+* @Author: Wendy Shu
+* @Date:   2017-07-04 11:30:53
+* @Last Modified by:   Wendy Shu
+* @Last Modified time: 2017-07-04 11:31:19
+*/
+
+'use strict';
+
+console.log('global');
---
+++ src/page/index/index.js
@@ -0,0 +1,12 @@
+/*
+* @Author: Wendy Shu
+* @Date:   2017-07-04 11:07:50
+* @Last Modified by:   Wendy Shu
+* @Last Modified time: 2017-07-04 13:21:23
+*/
+
+'use strict';
+
+require('./index.css');
+console.log("hello, index");
+// $('body').html('index hello~~~');
